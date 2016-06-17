3abd1e12929978dea6a483d34b93e468b4b30f10
Scott Steele <scott.steele@distilnetworks.com>
2016-06-17 14:44:33 -0400

    Use rspec's fancy `let` pattern

--- spec/reports/signatures_report_spec.rb
+++ spec/reports/signatures_report_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SignaturesReport, type: :report do
-  it 'generates a report based on the end of the week' do
-    end_date_string = '2016-05-09'
-    end_date = Date.parse(end_date_string)
-    report = SignaturesReport.for_week(ending: end_date)
+  let(:end_date_string) { '2016-05-09' }
+  let(:end_date) { Date.parse(end_date_string) }
+  let(:begin_date) { end_date - 6 }
+  let(:report) { SignaturesReport.for_week(ending: end_date) }
 
-    expect(report.begin).to eq(end_date - 6)
+
+  it 'instantiates a report based on the end of the week' do
+    expect(report.begin).to eq(begin_date)
     expect(report.end).to eq(end_date)
     expect(report.end - report.begin).to eq(6)
   end
