8ae59884073182e649e47e8fd35e0fd0ed68079d
Robb Kidd <robb@thekidds.org>
2016-06-17 16:54:23 -0400

    Add roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Never re-order this array
+  ROLES = %w[admin site_supervisor].freeze
+
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  end
+
+  def roles
+    ROLES.reject do |r|
+      ((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
+    end
+  end
+
+  def is?(role)
+    roles.include?(role.to_s)
+  end
 end
