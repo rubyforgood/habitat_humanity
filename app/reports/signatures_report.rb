800f515b8018ce0768bca45dafb7caf84ca66aeb
Scott Steele <scott.steele@distilnetworks.com>
2016-06-17 15:42:11 -0400

    Add SignaturesReport#data method

--- app/reports/signatures_report.rb
+++ app/reports/signatures_report.rb
@@ -15,4 +15,25 @@ class SignaturesReport
     begin_date = end_date - 6
     new(begin_date, end_date)
   end
+
+  ##
+  # @return [Hash{Date=>Array<ActiveRecord::Base>}]
+  def data
+    # Might not want to include a date entry for days without any data
+    out = (self.begin..self.end).map { |date| [date, []] }.to_h
+
+    # Get the *actual* data
+    out.merge(data_pull.to_a.group_by(&:day))
+  end
+
+  ##
+  # @private
+  #
+  # @return [ActiveRecord::Relation]
+  def data_pull
+    Shift.where(
+      'day BETWEEN :begin_date AND :end_date',
+      begin_date: @begin, end_date: @end
+    ).order(:day)
+  end
 end
