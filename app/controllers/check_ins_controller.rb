4f4d0dad05c951f2c1ba77ac3e51879729a1aea7
Pat Allan <pat@freelancing-gods.com>
2016-06-17 15:48:50 -0400

    Checking in works.

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,10 +1,20 @@
 class CheckInsController < ApplicationController
   expose(:check_in_form) { CheckInForm.new check_in_params }
 
+  def create
+    if check_in_form.valid?
+      check_in_form.save
+
+      redirect_to new_check_in_path, notice: "Check in has been saved."
+    else
+      render 'new', notice: "Check in is not valid"
+    end
+  end
+
   private
 
   def check_in_params
-    params.fetch(:check_in, {}).permit(
+    params.fetch(:check_in_form, {}).permit(
       :name, :email, :work_site_id, :signature
     )
   end
--- app/forms/check_in_form.rb
+++ app/forms/check_in_form.rb
@@ -5,7 +5,7 @@ class CheckInForm
 
   def initialize(attributes)
     @name         = attributes[:name]
-    @email        = attributes[:name]
+    @email        = attributes[:email]
     @work_site_id = attributes[:work_site_id]
     @signature    = attributes[:signature]
   end
@@ -18,9 +18,44 @@ class CheckInForm
     false
   end
 
+  def save
+    shift_event.save!
+  end
+
+  def valid?
+    volunteer.valid? && shift.valid? && shift_event.valid?
+  end
+
   def work_site
     return nil if work_site_id.nil?
 
     @work_site ||= WorkSite.find work_site_id
+  end
+
+  private
+
+  def shift
+    @shift ||= Shift.find_by(
+      volunteer_id: volunteer,
+      work_site_id: work_site,
+      day:          Date.today
+    ) || Shift.create(
+      volunteer: volunteer,
+      work_site: work_site,
+      day:       Date.today
+    )
+  end
+
+  def shift_event
+    @shift_event ||= shift.shift_events.build(
+      occurred_at: Time.current,
+      signature:   signature,
+      action:      'start_shift'
+    )
+  end
+
+  def volunteer
+    @volunteer ||= Volunteer.find_by(email: email) ||
+      Volunteer.create(name: name, email: email)
   end
 end
