37773cc7f8b7f34af8fbf57f8a6976b9bed94e1a
jen pengelly <jgpengelly@gmail.com>
2016-06-17 14:15:29 -0400

    adds user model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617150101) do
+ActiveRecord::Schema.define(version: 20160617181109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,24 @@ ActiveRecord::Schema.define(version: 20160617150101) do
   add_index "shifts", ["volunteer_id"], name: "index_shifts_on_volunteer_id", using: :btree
   add_index "shifts", ["work_site_id", "volunteer_id", "day"], name: "index_shifts_on_work_site_id_and_volunteer_id_and_day", unique: true, using: :btree
   add_index "shifts", ["work_site_id"], name: "index_shifts_on_work_site_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "volunteers", force: :cascade do |t|
     t.string   "name",       null: false
