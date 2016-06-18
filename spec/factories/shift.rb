0a74aa0165a459be2df1d313bc596ec1df78202d
Scott Steele <scott.steele@distilnetworks.com>
2016-06-17 23:41:54 -0400

    Simplify faking time for shift factory events

--- spec/factories/shift.rb
+++ spec/factories/shift.rb
@@ -7,7 +7,7 @@ FactoryGirl.define do
     trait :full do
       after(:build) do |shift|
         ShiftEvent::ACTIONS.zip(
-          Array.new(4) { Faker::Time.between(shift.day.to_datetime, shift.day.succ.to_datetime) }.sort
+          Array.new(4) { Faker::Time.between(shift.day, shift.day) }.sort
         ).each do |(action, occurred_at)|
           create(
             :shift_event,
