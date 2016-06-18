207496f5635487f1d97cd418611f3fcd9dcbe7cf
Scott Steele <scott.steele@distilnetworks.com>
2016-06-18 00:14:20 -0400

    Lousy attempt at validating shift events in a Shift
    Should maybe all be removed but wanted to commit before heading out for
    the night.

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -7,4 +7,37 @@ class Shift < ActiveRecord::Base
   validates :volunteer, presence: true
   validates :day,       presence: true,
                         uniqueness: { scope: [:work_site_id, :volunteer_id] }
+
+  # WIP
+  validates_each :shift_events do |record, attr, value|
+    if !record.started? && value.action != 'start_shift'
+      # Disallow
+    end
+
+    if record.started? && value.action == 'start_shift'
+      # Disallow addition of attr...only allow modification of attr
+    end
+
+    if record.ended? && value.action == 'end_shift'
+      # Disallow
+    end
+
+    # Disallow more things
+  end
+
+  def shift_start
+    shift_events.where(action: 'start_shift').first
+  end
+
+  def shift_end
+    shift_events.where(action: 'end_shift').first
+  end
+
+  def started?
+    !!shift_start
+  end
+
+  def ended?
+    !!shift_end
+  end
 end
