5c80917d891c485853ceb6bde4af935228e08267
Scott Steele <scott.steele@distilnetworks.com>
2016-06-18 00:10:35 -0400

    Shift has many `ShiftEvent`s

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,6 +1,7 @@
 class Shift < ActiveRecord::Base
   belongs_to :work_site
   belongs_to :volunteer
+  has_many   :shift_events
 
   validates :work_site, presence: true
   validates :volunteer, presence: true
