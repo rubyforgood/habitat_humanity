416655dc229964e72bfbca53d7616a91a2e3b9c3
Pat Allan <pat@freelancing-gods.com>
2016-06-18 11:53:36 -0400

    Remove unused worksites controller/views.

--- app/controllers/work_sites_controller.rb
+++
@@ -1,6 +0,0 @@
-class WorkSitesController < ApplicationController
-  expose(:work_sites) { WorkSite.all }
-  expose(:work_site) { WorkSite.find params.fetch(:id) }
-
-  expose(:check_in_form) { CheckInForm.new }
-end
