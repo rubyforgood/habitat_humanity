ecf1ada339ecc4274d670857dd3f0d9237c8bb12
Scott Steele <scott.steele@distilnetworks.com>
2016-06-17 23:27:34 -0400

    Add trait to create full shift (with all 4 shift event actions)

--- spec/factories/shift.rb
+++ spec/factories/shift.rb
@@ -3,5 +3,18 @@ FactoryGirl.define do
     day { Faker::Date.forward(23) }
     work_site
     volunteer { Volunteer.first || FactoryGirl.create(:volunteer) }
+
+    trait :full do
+      after(:build) do |shift|
+        ShiftEvent::ACTIONS.zip(
+          Array.new(4) { Faker::Time.between(shift.day.to_datetime, shift.day.succ.to_datetime) }.sort
+        ).each do |(action, occurred_at)|
+          create(
+            :shift_event,
+            action: action, occurred_at: occurred_at, shift: shift,
+          )
+        end
+      end
+    end
   end
 end
