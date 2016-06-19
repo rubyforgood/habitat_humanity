ed4d7dc875ae448d56ab27de7c6707b188e4ea9c
Robb Kidd <robb@thekidds.org>
2016-06-19 11:13:34 -0400

    Set default to tomorrow in the controller instead of the view
    It's silly to have 2 defaults.
    
    For the moment, the end-date needs to be tomorrow to include results from
    today. Then BETWEEN query uses end-date-at-midnight so would not include
    events from times during end-date that humans could reasonably be assumed
    to be starting or ending shifts.

--- app/controllers/signatures_reports_controller.rb
+++ app/controllers/signatures_reports_controller.rb
@@ -2,7 +2,7 @@ class SignaturesReportsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @end_date = Date.parse(params.fetch(:end_date, Time.zone.today.to_s))
+    @end_date = Date.parse(params.fetch(:end_date, 1.day.from_now.to_s))
     @begin_date = @end_date - 6
     @shift_events = SignaturesReport.for_week(ending: @end_date).pull_join
   end
