ac8416479fcb06cfcb938964b5d04554c8e449fc
Scott Steele <scott.steele@distilnetworks.com>
2016-06-19 10:54:30 -0400

    Pull report controller concern directly back into reports
    (We aren't yet sure how generic the report controllers can actually be.)

--- app/controllers/hours_report_controller.rb
+++ app/controllers/hours_report_controller.rb
@@ -1,6 +1,12 @@
-require_relative 'concerns/weekly_reportable_controller'
-
 class HoursReportController < ApplicationController
-  REPORT_CLASS = HoursReport
-  include WeeklyReportableController
+  before_action :authenticate_user!
+
+  def index
+    end_date = Date.parse(params.fetch(:end_date, Time.zone.today.to_s))
+    @report = HoursReport.for_week(ending: end_date)
+
+    respond_to do |format|
+      format.csv { send_data @report.to_csv, filename: @report.csv_filename }
+    end
+  end
 end
