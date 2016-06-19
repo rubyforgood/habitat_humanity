6f13021c5d02e697e1f090fbae3eebbc88a76b2d
Scott Steele <scott.steele@distilnetworks.com>
2016-06-19 11:10:36 -0400

    Generate hours report without being fancy with grabbing attrs

--- app/reports/hours_report.rb
+++ app/reports/hours_report.rb
@@ -10,29 +10,38 @@ class HoursReport
   def pull_join
     # TODO Have yet to get a Railsy query working here
     Shift
-      .select(*ATTRIBUTE_HEADERS)
-      .joins(:work_site, :volunteer, :shift_events)
+      .includes(:work_site, :volunteer, :shift_events)
       .where(day: @begin..@end)
       .order(:day)
       .select(&:complete?)
   end
 
-  ATTRIBUTE_HEADERS = %w(address
-                         day
-                         name).freeze
-
-  JOINED_HEADERS = (ATTRIBUTE_HEADERS + %w(
-                         shift_start_time
-                         shift_end_time
-                         duration
-                         duration_without_breaks)).freeze
+  JOINED_HEADERS = %w(address
+                      day
+                      name
+                      email
+                      shift_start_time
+                      shift_end_time
+                      duration
+                      duration_without_breaks).freeze
 
   def to_csv
-    CSV.generate(write_headers: false, headers: self.class::JOINED_HEADERS) do |csv|
+    CSV.generate(write_headers: false, headers: JOINED_HEADERS) do |csv|
       # Don't want to rely on `write_headers: true` since we want still
       # header row in the CSV file even when there is no data.
-      csv << self.class::JOINED_HEADERS
-      pull_join.each { |record| csv << self.class::JOINED_HEADERS.map(&record.method(:send)) }
+      csv << JOINED_HEADERS
+      pull_join.each do |record|
+        csv << [
+          record.work_site.address,
+          record.day,
+          record.volunteer.name,
+          record.volunteer.email,
+          record.shift_start_time,
+          record.shift_end_time,
+          record.duration,
+          record.duration_without_breaks,
+        ]
+      end
     end
   end
 end
