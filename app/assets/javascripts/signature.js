33e94d83a3e367b8cafc49a5b3416a1545f8e01d
Brandon Lawrence <brandon.lawrence@customink.com>
2016-06-19 09:56:38 -0400

    update check in form to disable signtures on breaks

--- app/assets/javascripts/signature.js
+++ app/assets/javascripts/signature.js
@@ -1,6 +1,19 @@
 //= require signature_pad
 $( document ).ready(function() {
   $('select').material_select();
+  $( ".dropdown-content span:contains('Break')" ).append( " <sup class='Signature-sup'>* signature not required</sup>" );
+  $( ".dropdown-content span:contains('Break')" ).closest('li').addClass('break')
+  $(".select-dropdown li").on('click', function(e) {
+    $span = $(e.target)
+    if($span.closest('li').hasClass('break')){
+      $('.Signature-visable').css('display', 'none')
+      $('.signature-field').val('N/A')
+    } else {
+      $('.Signature-visable').css('display', 'block')
+      $('.signature-field').val('')
+    }
+  })
+
 
   var wrapper = document.getElementById("Signature"),
       clearButton = document.getElementById("clear"),
@@ -15,7 +28,7 @@ $( document ).ready(function() {
   });
 
   $('#new_check_in_form').submit(function(e) {
-    if (signaturePad.isEmpty()) {
+    if ($('.signature-field').val('')) {
       $('.Signature-pad').addClass('Signature-pad-error');
       $('.description').addClass('Signature-error');
       return false;
