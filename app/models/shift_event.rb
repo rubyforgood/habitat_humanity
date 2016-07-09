b378ab2abb3b4786080e240b1ca1d2ef44b5ce48
Ben Miller <bjmllr@gmail.com>
2016-07-09 19:23:17 -0400

    validate ShiftEvent.occurred_at via Shift.day
    Fixes #68

--- app/models/shift_event.rb
+++ app/models/shift_event.rb
@@ -1,4 +1,6 @@
 class ShiftEvent < ActiveRecord::Base
+  include ActiveModel::Validations
+
   ACTIONS = {
     'start_shift' => 'Start Shift',
     'start_break' => 'Start Break',
@@ -11,6 +13,13 @@ class ShiftEvent < ActiveRecord::Base
   validates :shift,       presence: true
   validates :action,      presence: true, inclusion: { in: ACTIONS }
   validates :occurred_at, presence: true
+  validate :shift_day_matches_occurred_at
 
   delegate :address, :day, :volunteer_name, :volunteer_email, :minor, to: :shift
+
+  def shift_day_matches_occurred_at
+    return if day == occurred_at.to_date
+    msg = "occurred_at (#{occurred_at.to_date}) does not match day (#{day})"
+    errors.add(:day, msg)
+  end
 end
