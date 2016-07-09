2e8546cf0d5ffa2ec768d6ab9f835cbef1203f8a
Lee Sharma <lee@leesharma.com>
2016-07-09 23:52:22 +0000

    Add shift events to db seeds
    Also fix shift_event factory to set `ShiftEvent#occurred_at` to be on
    the same date as associated `Shift#day`

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,6 @@ require 'factory_girl'
 
 10.times do
   FactoryGirl.create(:work_site)
-  FactoryGirl.create(:shift)
+  shift = FactoryGirl.create(:shift)
+  FactoryGirl.create(:shift_event, shift: shift)
 end
