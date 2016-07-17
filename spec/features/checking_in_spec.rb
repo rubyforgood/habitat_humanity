9a28d413d3ed6775a1453347797ff7c7e01bd665
Ben Miller <bjmllr@gmail.com>
2016-07-16 21:54:12 -0400

    test the activation of lolliclock on the main form

--- spec/features/checking_in_spec.rb
+++ spec/features/checking_in_spec.rb
@@ -25,4 +25,24 @@ RSpec.describe 'Checking in at a worksite', type: :feature do
     expect(shift_event.action).to eq('start_shift')
     # expect(shift_event.occurred_at).to eq(Time.zone.local(2016, 1, 1, 16, 30))
   end
+
+  # Given the main form has just loaded
+  # Then I do not see the lolliclock widget
+  scenario 'hides the lolliclock widget when the form loads', js: true do
+    skip 'PhantomJS 2+ required' unless ENV['RAILS_SPEC_JS']
+    visit root_path
+    expect { find(:css, '.lolliclock-popover') }
+      .to raise_error Capybara::ElementNotFound
+  end
+
+  # Given the main form has just loaded
+  # When I click the time input field
+  # Then I see the lolliclock widget
+  scenario 'activates lolliclock when time input is focused', js: true do
+    skip 'PhantomJS 2+ required' unless ENV['RAILS_SPEC_JS']
+    visit root_path
+    page.execute_script('document.getElementById("pick-a-time").click()')
+    clock = find(:css, '.lolliclock-popover')
+    expect(clock).to be_visible
+  end
 end
