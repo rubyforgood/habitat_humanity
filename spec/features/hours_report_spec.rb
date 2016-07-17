4328e96e34a4e83ae1379b82311047a03d15baa3
Lee Sharma <lee@leesharma.com>
2016-07-17 02:02:43 +0000

    Appease rubocop
    It found the whitespace displeasing.

--- spec/features/hours_report_spec.rb
+++ spec/features/hours_report_spec.rb
@@ -6,7 +6,7 @@ feature 'Admin can download hours report', :feature do
     visit hours_report_path(format: :csv)
 
     expect(current_path).to eq '/admin/hours_report'
-    
+
     expect(page.status_code).to eq 200
     expect(page.response_headers['Content-Type']).to eq 'text/csv'
   end
